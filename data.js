efd1ef3ed10c80b3da1c2f3e64bd27abd3d24f0d
ar135 <archat71@gmail.com>
2021-07-26 16:15:48 -0400

    changes

---
+++ data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'name 1',
+    job: 'product customer',
+    image:
+      '1.jpg',
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus et molestie ac feugiat sed lectus. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Euismod in pellentesque massa placerat duis. Integer quis auctor elit sed vulputate. Vitae auctor eu augue ut lectus arcu bibendum at. Tempus egestas sed sed risus pretium quam. Et egestas quis ipsum suspendisse ultrices gravida. Diam maecenas ultricies mi eget. Vivamus arcu felis bibendum ut tristique et egestas quis. Vivamus arcu felis bibendum ut tristique et egestas quis ipsum. Orci a scelerisque purus semper. A iaculis at erat pellentesque adipiscing commodo elit. Et malesuada fames ac turpis egestas.",
+  },
+  {
+    id: 2,
+    name: 'name 2',
+    job: 'Clothes Product Customer',
+    image:
+      '2.jpg',
+    text:
+      'Quisque egestas diam in arcu cursus euismod quis viverra. Sit amet dictum sit amet justo donec enim diam. Morbi leo urna molestie at elementum eu facilisis sed odio. Aliquet sagittis id consectetur purus ut faucibus. Risus feugiat in ante metus. Vitae sapien pellentesque habitant morbi tristique senectus et netus et. Ipsum consequat nisl vel pretium lectus. Non sodales neque sodales ut etiam sit amet nisl. In tellus integer feugiat scelerisque.',
+  },
+  {
+    id: 3,
+    name: 'name 3',
+    job: 'Technology Product Customer',
+    image:
+      '3.jpg',
+    text:
+      'Eu mi bibendum neque egestas congue quisque egestas. A scelerisque purus semper eget duis at. Magnis dis parturient montes nascetur ridiculus mus mauris. Interdum varius sit amet mattis vulputate enim nulla aliquet. Sit amet nisl purus in mollis nunc sed id. Ullamcorper a lacus vestibulum sed. Urna duis convallis convallis tellus id interdum velit.',
+  },
+  {
+    id: 4,
+    name: 'name 4',
+    job: 'customer',
+    image:
+      '4.jpg',
+    text:
+      'n aliquam sem fringilla ut morbi. Mi eget mauris pharetra et ultrices neque ornare. Enim nunc faucibus a pellentesque sit. Sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Tristique risus nec feugiat in fermentum posuere urna. Consectetur adipiscing elit ut aliquam purus sit amet luctus. Convallis posuere morbi leo urna molestie at. Dui vivamus arcu felis bibendum ut tristique et.',
+  },
+];
+
+export default reviews;
